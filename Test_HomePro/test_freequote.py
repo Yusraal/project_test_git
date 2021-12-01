f41decf9aaed0a6897a889d6333ca03d614989a4
Yusra <alimyyusra@gmail.com>
2021-12-01 11:19:12 -0500

    name element was added it to the test script.

--- Test_HomePro/test_freequote.py
+++ Test_HomePro/test_freequote.py
@@ -1,6 +1,11 @@
+import time
+
 import pytest
 
-
+@pytest.mark.smoketest
 def test_freequote(browser):
     browser.get("https://homepro.herokuapp.com/quote.php")
+    assert browser.title == "HomePro, Inc"
+    print(browser.title)
     first_name = browser.find_element_by_name("first_name").send_keys("Ahmad")
+    time.sleep(3)
